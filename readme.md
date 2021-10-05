cd823e469f0be0035c7099b77d104b7ce5f0dc0f
mslancer <wolfnw@yandex.ru>
2021-10-05 23:01:19 +0500

    Final

---
+++ readme.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/t5c88v468onqnvsn?svg=true)](https://ci.appveyor.com/project/mslancer/alphabank)
--- src/test/java/ru/netology/web/RegBankTest.java
+++ src/test/java/ru/netology/web/RegBankTest.java
@@ -1,6 +1,5 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -9,15 +8,85 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class RegBankTest {
 
-        @Test
-        void shouldTest() {
-            open("http://localhost:7777");
-            SelenideElement form = $("[data-test-id=callback-form]");
-            form.$("[data-test-id=name] input").setValue("Василий");
-            form.$("[data-test-id=phone] input").setValue("+79270000000");
-            form.$("[data-test-id=agreement]").click();
-            form.$("[data-test-id=submit]").click();
-            $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
-        
+    @Test
+    void shouldTestValid() {
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Кузьмин Владислав");
+        $("[data-test-id=phone] input").setValue("+79276380430");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldTestValidDoubleName() {
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Кузьмин Владислав-Владимир");
+        $("[data-test-id=phone] input").setValue("+79276380430");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+
+    }
+
+    @Test
+    void shouldTestValidDoubleSurname() {
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Кузьмин-Кузнецов Владислав");
+        $("[data-test-id=phone] input").setValue("+79276380430");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+
+    }
+
+    @Test
+    void shouldTestInvalidNumber() {
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Кузьмин Владислав");
+        $("[data-test-id=phone] input").setValue("+792763804300");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+
+    }
+
+    @Test
+    void shouldEnglishName() {
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Kuzmin Vladislav");
+        $("[data-test-id=phone] input").setValue("+79276380430");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+
+    }
+
+    @Test
+    void shouldNameWithoutSpace() {
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("КузьминВладислав");
+        $("[data-test-id=phone] input").setValue("+79276380430");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+
+    }
+
+    @Test
+    void shouldNameWithoutSurname() {
+        open("http://localhost:7777");
+        $("[data-test-id=name] input").setValue("Кузьмин");
+        $("[data-test-id=phone] input").setValue("+79276380430");
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+
     }
 }
